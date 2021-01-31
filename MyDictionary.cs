970e476bfa62e1552a075e04bdb9021df1f4de10
YazarBayan <78087142+YazarBayan@users.noreply.github.com>
2021-01-31 04:03:20 +0300

    Add files via upload

---
+++ MyDictionary.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionaryApp
+{
+    public class MyDictionary<K,V>
+    {
+        K[] _key;
+        V[] _value;
+        K[] _tKey;
+        V[] _tValue;
+        public MyDictionary()
+        {
+            _key = new K[0];
+            _value = new V[0];
+        }
+        public void Add(K key, V value)
+        {
+            _tKey = _key;
+            _tValue = _value;
+
+            _key = new K[_key.Length + 1];
+            _value = new V[_value.Length + 1];
+
+            for (int i = 0; i < _tKey.Length; i++)
+            {
+                _key[i] = _tKey[i];
+            }
+            for (int i = 0; i < _tValue.Length; i++)
+            {
+                _value[i] = _tValue[i];
+            }
+
+            _key[_key.Length - 1] = key;
+            _value[_value.Length - 1] = value;
+        }
+        public int Count
+        {
+            get { return _value.Length; }
+        }
+        public K[] Key
+        {
+            get { return _key; }
+        }
+
+        public V[] Value
+        {
+            get { return _value; }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MyDictionaryApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int,string> kullanicilar = new MyDictionary<int, string>();
+            //Diğer listelerde olduğu gibi Add() metodu ile ekleme yapıyoruz..
+            kullanicilar.Add(1, "Deniz TENİK");
+            kullanicilar.Add(2, "Merve ŞEKERCİ");
+            kullanicilar.Add(3, "Alper KAYA");
+            kullanicilar.Add(4, "Sezai ÇİNAR");
+            kullanicilar.Add(5, "Sema YAŞAR");
+            kullanicilar.Add(6, "Gizem TINAZ");
+            kullanicilar.Add(7, "Banu ÇİNAR");
+
+            Console.WriteLine("Listedeki Kullanıcı Sayısı: "+ kullanicilar.Count);
+            for (int i = 0; i < kullanicilar.Count; i++)
+            {
+                Console.WriteLine(kullanicilar.Key[i]+" "+kullanicilar.Value[i]);
+            }
+
+
+            Console.ReadLine();
+      
+        }
+    }
+}
